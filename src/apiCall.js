e56e35399109caf881cdaa790f150ed6992c7aab
MisterKrud <162855452+MisterKrud@users.noreply.github.com>
2025-08-31 15:27:37 +1000

    Clean up code

--- src/apiCall.js
+++ src/apiCall.js
@@ -1,9 +1,7 @@
 import { dataComponents } from "./dataComponents";
-import { format, fromUnixTime, getDay} from "date-fns";
+import { format, fromUnixTime, getDay } from "date-fns";
 import { getData } from "./userInput";
 // import { location } from "./geoLocation";
-
-
 
 const components = dataComponents();
 
@@ -20,136 +18,126 @@ const weatherInfo = () => {
   const location = () => navigator.geolocation.getCurrentPosition(success);
 
   const coordinates = location();
-
-}
+};
 export function getWeatherData(
-    loc = "Sydney",
-    dateA = "",
-    dateB = "",
-    unit = "metric"
-  ) {
+  loc = "Sydney",
+  dateA = "",
+  dateB = "",
+  unit = "metric"
+) {
+  let deg;
 
-    let deg 
+  console.log("^^getWeatherData");
+  console.log(loc, dateA, dateB, unit);
 
-    
-    console.log("^^getWeatherData");
-    console.log(loc, dateA, dateB, unit);
+  const getWeather = async () => {
+    console.log(`date1 initial value: ${dateA}`);
+    console.log(`date2 initial value: ${dateB}`);
 
-    const getWeather = async () => {
-      console.log(`date1 initial value: ${dateA}`);
-      console.log(`date2 initial value: ${dateB}`);
+    let date1;
+    let date2;
 
-      let date1;
-      let date2;
-
-      const getDate1 = (() => {
-        if (dateA != "") {
-          date1 = "/" + dateA;
-        } else {
-          date1 = "";
-        }
-        console.log(date1 + dateA);
-        return date1;
-      })();
-
-      const getDate2 = (() => {
-        if (dateB != "") {
-          date2 = "/" + dateB;
-        } else {
-          date2 = "";
-        }
-        console.log(date2 + dateB);
-        return date2;
-      })();
-
-      console.log(
-        `${components.baseURL}${loc}${getDate1}${getDate2}?unitGroup=${unit}&include=current&key=J5R7RYMK57B597QLPD9UF4W8Y&contentType=json`
-      );
-
-      const response = await fetch(
-        `${components.baseURL}${loc}${getDate1}${getDate2}?unitGroup=${unit}&include=current&key=J5R7RYMK57B597QLPD9UF4W8Y&contentType=json`,
-        { mode: "cors" }
-      );
-     
-      const weatherData = await response.json();
-   
-
-      console.log("function activated");
-      console.log(weatherData);
-      console.log(`Date range: ${dateA} to ${dateB}`);
-      console.log(`Location: ${weatherData.address}`);
-    
-      try {
-        console.log(
-          `Currently  ${weatherData.currentConditions.temp} degrees and ${weatherData.currentConditions.conditions}`
-        );
-      } catch (error) {
-        console.log("No current temp information");
-      }
-         if (unit === "uk" || unit === "us" ) {
-       deg = '\u00B0F'
-       
-    } else {
-       deg = '\u00B0C'
-      
-    }
-
-      let weekDays = []
-      //   if (unit = "US" || "UK"){
-      //   const deg = '\u00B0F'
-      // } else {
-        // const deg = '\u00B0C'
-      // }
-      //CONSOLE LOG 7 DAY FORECAST
-     for (let i=0; i< 7; i++){
-      const data = weatherData.days
-      const dateFromUnix = fromUnixTime(weatherData.days[i].datetimeEpoch)
-      const weekDay = format(dateFromUnix, 'eeee');
-        weekDays.push(weekDay)
-      if (i === 0){
-        console.log('')
-        console.log(`${loc.toUpperCase()} 7 DAY FORECAST`);
-        console.log(`${weekDay} (Today): ${data[i].conditions} -> ${data[i].tempmin}${deg} - ${data[i].tempmax}${deg}`);
-      } else if (i ===1){
-        console.log(`${weekDay} (Tomorrow): ${data[i].conditions} ->  ${data[i].tempmin}${deg} - ${data[i].tempmax}${deg}`);
+    const getDate1 = (() => {
+      if (dateA != "") {
+        date1 = "/" + dateA;
       } else {
-        console.log(`${weekDay}: ${data[i].conditions} ->  ${data[i].tempmin}${deg} - ${data[i].tempmax}${deg}`);
+        date1 = "";
+      }
+      console.log(date1 + dateA);
+      return date1;
+    })();
+
+    const getDate2 = (() => {
+      if (dateB != "") {
+        date2 = "/" + dateB;
+      } else {
+        date2 = "";
+      }
+      console.log(date2 + dateB);
+      return date2;
+    })();
+
+    console.log(
+      `${components.baseURL}${loc}${getDate1}${getDate2}?unitGroup=${unit}&include=current&key=J5R7RYMK57B597QLPD9UF4W8Y&contentType=json`
+    );
+
+    const response = await fetch(
+      `${components.baseURL}${loc}${getDate1}${getDate2}?unitGroup=${unit}&include=current&key=J5R7RYMK57B597QLPD9UF4W8Y&contentType=json`,
+      { mode: "cors" }
+    );
+
+    const weatherData = await response.json();
+
+    console.log("function activated");
+    console.log(weatherData);
+    console.log(`Date range: ${dateA} to ${dateB}`);
+    console.log(`Location: ${weatherData.address}`);
+
+    try {
+      console.log(
+        `Currently  ${weatherData.currentConditions.temp} degrees and ${weatherData.currentConditions.conditions}`
+      );
+    } catch (error) {
+      console.log("No current temp information");
+    }
+    if (unit === "uk" || unit === "us") {
+      deg = "\u00B0F";
+    } else {
+      deg = "\u00B0C";
+    }
+
+    let weekDays = [];
+   
+    for (let i = 0; i < 7; i++) {
+      const data = weatherData.days;
+      const dateFromUnix = fromUnixTime(weatherData.days[i].datetimeEpoch);
+      const weekDay = format(dateFromUnix, "eeee");
+      weekDays.push(weekDay);
+      if (i === 0) {
+        console.log("");
+        console.log(`${loc.toUpperCase()} 7 DAY FORECAST`);
+        console.log(
+          `${weekDay} (Today): ${data[i].conditions} -> ${data[i].tempmin}${deg} - ${data[i].tempmax}${deg}`
+        );
+      } else if (i === 1) {
+        console.log(
+          `${weekDay} (Tomorrow): ${data[i].conditions} -> ${data[i].tempmin}${deg} - ${data[i].tempmax}${deg}`
+        );
+      } else {
+        console.log(
+          `${weekDay}: ${data[i].conditions} -> ${data[i].tempmin}${deg} - ${data[i].tempmax}${deg}`
+        );
       }
     }
 
-   //CONSOLE LOG TODAY'S WEATHER DETAILS
-   const today = weatherData.days[0]
-   console.log('');
-   console.log("TODAY'S DETAILS");
-   console.log(today.description);
-   console.log('');
-   console.log(`TEMP`);
-   console.log(`Now: ${today.temp}${deg} (feels like ${today.feelslike}${deg})`);
-   console.log(`Max: ${today.tempmax}${deg} (feels like ${today.feelslikemax}${deg})`);
-   console.log(`Min: ${today.tempmin}${deg} (feels like ${today.feelslikemin}${deg})`);
-   console.log('');
-   console.log(`Sunrise: ${today.sunrise}`);
-   console.log(`Sunset: ${today.sunset}`);
-   console.log('');
-    console.log(`Chance of rain: ${today.precipprob}`)
-  console.log('');
-   console.log(`Humidity: ${today.humidity}`)
-   console.log(`Wind speed: ${today.windspeed}`)
-   console.log(`UV Index: ${today.uvindex}`)
+    //CONSOLE LOG TODAY'S WEATHER DETAILS
+    const today = weatherData.days[0];
+    console.log("");
+    console.log("TODAY'S DETAILS");
+    console.log(today.description);
+    console.log("");
+    console.log(`TEMP`);
+    console.log(
+      `Now: ${today.temp}${deg} (feels like ${today.feelslike}${deg})`
+    );
+    console.log(
+      `Max: ${today.tempmax}${deg} (feels like ${today.feelslikemax}${deg})`
+    );
+    console.log(
+      `Min: ${today.tempmin}${deg} (feels like ${today.feelslikemin}${deg})`
+    );
+    console.log("");
+    console.log(`Sunrise: ${today.sunrise}`);
+    console.log(`Sunset: ${today.sunset}`);
+    console.log("");
+    console.log(`Chance of rain: ${today.precipprob}%`);
+    console.log("");
+    console.log(`Humidity: ${today.humidity}%`);
+    console.log(`Wind speed: ${today.windspeed} km/h`);
+    console.log(`UV Index: ${today.uvindex}`);
 
-    
+    return weatherData;
+  };
 
-
-
-
-
-     
-   
-      
-
-      return weatherData;
-    };
-
-    getWeather();
-  }
-
+  getWeather();
+}
--- src/userInput.js
+++ src/userInput.js
@@ -1,59 +1,53 @@
 import { getWeatherData } from "./apiCall";
 
-// const city = document.getElementById("location");
-// const citySubmit = document.getElementById("city-button");
-
-// export const location = () => {
-//   citySubmit.addEventListener("click", () => getWeatherData(city.value));
-// };
-
-// const unit = document.getElementById("unit");
-// const unitSubmit = document.getElementById("unit-submit");
-
-// export const units = () => {
-//   unitSubmit.addEventListener("click", () => getWeatherData(unit.value));
-// };
-
 const allInputs = Array.from(document.querySelectorAll("input"));
 const locationInput = document.getElementById("location");
-const unitInput = document.getElementById("unit")
+const unitInput = document.getElementById("unit");
 
 const submitButton = document.getElementById("submit-all");
 const locationButton = document.getElementById("location-button");
 const unitButton = document.getElementById("unit-button");
 
-let inputValues = []
-
+let inputValues = [];
 
 export const getData = () => {
   submitButton.addEventListener("click", () => {
-    inputValues=[];
+    inputValues = [];
     allInputs.forEach((input) => {
       inputValues.push(input.value);
     });
-        getWeatherData(inputValues[0],inputValues[1],inputValues[2], inputValues[3])
-        console.log(inputValues)
-        
+    getWeatherData(
+      inputValues[0],
+      inputValues[1],
+      inputValues[2],
+      inputValues[3]
+    );
+    console.log(inputValues);
   });
-  
 };
 
-export const getLocation = () =>{
-  locationButton.addEventListener("click", () =>{
-    inputValues[0] = locationInput.value
-    getWeatherData(inputValues[0],inputValues[1],inputValues[2],inputValues[3] )
-  })
+export const getLocation = () => {
+  locationButton.addEventListener("click", () => {
+    inputValues[0] = locationInput.value;
+    getWeatherData(
+      inputValues[0],
+      inputValues[1],
+      inputValues[2],
+      inputValues[3]
+    );
+  });
+};
 
-}
-
-export const getUnit = () =>{
-  unitButton.addEventListener("click", ()=> {
+export const getUnit = () => {
+  unitButton.addEventListener("click", () => {
     inputValues[3] = unitInput.value;
-    getWeatherData(inputValues[0],inputValues[1],inputValues[2], inputValues[3])
-  })
-
-}
-
-//https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/null/null/null?unitGroup=uk&include=current&key=J5R7RYMK57B597QLPD9UF4W8Y&contentType=json
+    getWeatherData(
+      inputValues[0],
+      inputValues[1],
+      inputValues[2],
+      inputValues[3]
+    );
+  });
+};
 
 
